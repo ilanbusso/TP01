5c82e0b646535c5a6387785ea393f1d2a496c944
ILÁN URIEL BUSSO <48857364@ort>
2026-03-11 11:10:34 -0300

    primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP01.Models;
+
+namespace TP01.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
